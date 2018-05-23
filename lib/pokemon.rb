c941834ed2d87dc2297480514ffc3ce5c4efb06a
Megan Dotto <megan.dotto@gmail.com>
2018-05-23 01:52:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,4 +13,13 @@ class Pokemon
      db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
 
+  def self.find(id_number, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id_number).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+  end
+
 end
